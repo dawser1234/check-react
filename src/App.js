07219dae6d2c00265876c68c015693af0c4c3da6
dawser1234 <dawser.ghzel987@gmail.com>
2022-08-20 12:37:14 +0100

    msg

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import proj from "./img/proj1(1).jpg"
 import proj2 from "./img/proj2.jpg"
 import proj3 from "./img/proj3.jpg"
 import proj4 from "./img/proj4.jpg"
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import ('https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe&display=swap');
 function App() {
   return (
    
   <div>
-    <header>
+   <header>
       <h1>John Doe</h1>
       <ul class="navmenu">
         <li><a href="#">About</a></li>
@@ -18,6 +20,45 @@ function App() {
         <li><a href="#">Contact</a></li>
       </ul>
     </header>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={proj4}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={proj}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={proj2}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     <section id="about-me">
       <h1>
         Hello, my name is
